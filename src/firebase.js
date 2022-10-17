278f2c3a8d871730e1b5c3859cffa7c9226d36da
IMMJ <story_mania@naver.com>
2022-10-17 14:40:31 +0900

    2022-10-17

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAvX9RONR8_nmKi5q3M26zOTJpF_Unl16o",
-    authDomain: "discord-331e2.firebaseapp.com",
-    projectId: "discord-331e2",
-    storageBucket: "discord-331e2.appspot.com",
-    messagingSenderId: "630870928080",
-    appId: "1:630870928080:web:65e48ce7fab4edfc2e4cd5",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
